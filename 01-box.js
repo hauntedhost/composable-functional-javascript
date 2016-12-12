e30d9aecf1eb35073cf9ada7729b323db62d3fd7
Sean Omlor <somlor@eml.cc>
2016-12-12 00:25:34 -0500

    first lesson

---
+++ 01-box.js
@@ -0,0 +1,16 @@
+// identity functor ^__^
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`, // neat
+});
+
+const charFromNumericString = str =>
+  Box(str)
+    .map(s => s.trim())
+    .map(s => parseInt(s))
+    .map(n => n + 1)
+    .map(i => String.fromCharCode(i))
+    // .fold(i => String.fromCharCode(i))
+
+console.log(charFromNumericString(' 64.0 '))
