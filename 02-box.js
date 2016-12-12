2f87e8cfbd9ac059f3525ae619fd911a1d524e4c
Sean Omlor <somlor@eml.cc>
2016-12-12 01:16:38 -0500

    more composition fun

---
+++ 02-box.js
@@ -0,0 +1,28 @@
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`,
+});
+
+const moneyToFloat = str =>
+  Box(str)
+  .map(s => s.replace(/\$/g, ''))
+  .map(r => parseFloat(r))
+
+const percentToFloat = str =>
+  Box(str)
+  .map(s => s.replace(/\%/g, ''))
+  .map(s => parseFloat(s))
+  .map(n => n * 0.01)
+
+console.log(moneyToFloat('$10.50'))
+console.log(percentToFloat('12.25%'))
+
+const applyDiscount = (priceStr, discountStr) =>
+  moneyToFloat(priceStr)
+  .fold(price =>
+    percentToFloat(discountStr)
+    .fold(discount =>
+      price - price * discount))
+
+console.log(applyDiscount('$20', '10%'))
