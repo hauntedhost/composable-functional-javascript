6830c4fd1e5298e9314ae6f613f1056313c04167
Sean Omlor <somlor@eml.cc>
2016-12-15 03:11:34 -0500

    monoids and semigroups

---
+++ 08-monoids.js
@@ -0,0 +1,39 @@
+const { compose, concat, uniq } = require('lodash/fp');
+
+const Sum = x => ({
+  x,
+  concat: ({x: y}) => Sum(x + y),
+  inspect: _ => `Sum(${x})`,
+});
+
+Sum.empty = _ => Sum(0);
+
+const sum = Sum.empty()
+  .concat(Sum(3))
+  .concat(Sum(5));
+
+console.log(sum);
+
+const All = x => ({
+  x,
+  concat: ({x: y}) => All(x && y),
+  inspect: _ => `All(${x})`,
+});
+
+All.empty = _ => All(true);
+
+const all = All.empty()
+  .concat(All(false))
+  .concat(All(true));
+
+console.log(all);
+
+const First = x => ({
+  x,
+  concat: _ => First(x),
+  inspect: _ => `First(${x})`,
+});
+
+// hmmm, no way to define a neutral element that is first and has no effect
+// seems like this must remain a semigroup instead of a monoid
+// First.empty = _ => First(???)
